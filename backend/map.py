a495e0d73ac83a707b8ca7656b982f5df65e9067
myokaiser <wills209@hotmail.fr>
2026-06-17 19:24:34 +0200

    fix: remove dimacs and gophersat functions

--- backend/map.py
+++ backend/map.py
@@ -159,40 +159,6 @@ def all_cases_certaines(nb_lignes, nb_colonnes) -> List :
 
 #-----------------------------AFFICHAGE MAP------------------------------------------------
 
-#-----------------------------FONCTIONS DE FICHIER--------------------------------------------
-def clauses_to_dimacs(clauses: ClauseBase, nb_vars: int) -> str:
-    dimacs = str()
-    nb_clauses = 0
-    for clause in clauses : 
-        for el in clause : 
-            dimacs += str(el) + " "
-        dimacs += "0\n"
-        nb_clauses += 1
-    result = 'p cnf ' + str(nb_vars) +' '+ str(nb_clauses) + '\n' + dimacs
-    return result
-
-def write_dimacs_file(dimacs: str, filename: str):
-    with open(filename, "w", newline="") as cnf:
-        cnf.write(dimacs)
-
-def clean_file():
-    os.remove("cnf_directory/known_case.cnf")
-
-def exec_gophersat(filename: str, cmd: str = "./gophersat.exe", encoding: str = "utf8") -> Tuple[bool, List[int]]:
-    result = subprocess.run(
-        [cmd, filename], capture_output=True, check=True, encoding=encoding
-    )
-    string = str(result.stdout)
-    lines = string.splitlines()
-    if lines[1] != "s SATISFIABLE":
-        return False, []
-
-    model = lines[2][2:-2].split(" ")
-
-    return True, [int(x) for x in model]
-#-----------------------------FONCTIONS DE FICHIER--------------------------------------------
-
-
 #-----------------------------FONCTIONS SUR LES CONTRAINTES--------------------------------------------
 
 def at_least_one(variables: List[PropositionnalVariable]) -> Clause : 
