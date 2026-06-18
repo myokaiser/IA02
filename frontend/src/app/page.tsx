a0b69efd053f82be8474e6a52f89e5a4e55f8446
myokaiser <wills209@hotmail.fr>
2026-06-18 10:56:35 +0200

    feat: updating main page with a scroll list of the options

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,75 @@
 "use client";
 
 import GameButton from "@/src/components/GameButton";
+import SelectMapButton from "@/src/components/SelectMapButton";
+import MotivationButton from "@/src/components/MotivationButton";
 import GitHubButton from "@/src/components/GitHubButton";
 
 export default function Home() {
-
   return (
-    <div className="p-6 flex flex-col gap-4 bg-black min-h-screen text-white">
+    <main className="h-screen bg-black text-white overflow-hidden">
+      <div className="flex flex-col items-center justify-center h-full">
 
-      <div className="flex flex-col items-center justify-center min-h-screen bg-black gap-6">
-        <div className="scale-[3] origin-center">
+        {/* Titre */}
+        <h1 className="
+          scale-[4] origin-center
+          mb-12
+          select-none
+        ">
           HITMAN.
-        </div>
-        <div className="flex flex-col gap-1 items-center">
-          <GameButton />
-          <GitHubButton /> 
-        </div>
+        </h1>
 
+        {/* Liste scrollable */}
+        <div className="relative">
+
+          {/* Fade inférieur */}
+          <div
+            className="
+              pointer-events-none
+              absolute
+              bottom-0
+              left-0
+              right-0
+              h-15
+              bg-gradient-to-b
+              from-transparent
+              to-black
+              z-10
+            "
+          />
+
+          <div
+            className="
+              h-25
+              w-72
+              overflow-y-auto
+              scrollbar-none
+              snap-y
+              snap-mandatory
+            "
+          >
+            <div className="flex flex-col items-center gap-1 pb-30">
+
+              <div className="snap-center">
+                <GameButton />
+              </div>
+
+              <div className="snap-center">
+                <SelectMapButton />
+              </div>
+
+              <div className="snap-center">
+                <MotivationButton />
+              </div>
+
+              <div className="snap-center">
+                <GitHubButton />
+              </div>
+
+            </div>
+          </div>
+        </div>
       </div>
-
-    </div>
+    </main>
   );
 }
