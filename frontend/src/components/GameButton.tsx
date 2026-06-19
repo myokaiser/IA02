4b9096fc21a9bbd8845153a1a50a1083ffc92669
myokaiser <wills209@hotmail.fr>
2026-06-19 12:03:51 +0200

    fix: cleaning button files

--- frontend/src/components/GameButton.tsx
+++ frontend/src/components/GameButton.tsx
@@ -4,17 +4,16 @@ import { useRouter } from "next/navigation";
 export default function GameButton() {
   const router = useRouter();
   const handleSignup = () => {
-    router.push("/game"); // page custom
+    router.push("/game");
   };
 
   return (
     <div className="flex items-center gap-3">
       <button
         onClick={handleSignup}
-        className={`
-          px-4 py-2 rounded-md font-mono text-sm cursor-pointer
-          bg-zinc-700 text-white
-        `}
+        className="
+          px-4 py-2 rounded-md font-mono text-sm cursor-pointer transition hover:bg-zinc-800 bg-zinc-700 text-white
+        "
       >
         {"▶ Start"}
       </button>
